3bdb502361174669b5340a0c4b91730ec8f7fe86
Pingky Oktiawati <pingkyoktiawati@gmail.com>
2024-01-21 10:59:57 +0700

    add routing for notes api

--- app/api/main.py
+++ app/api/main.py
@@ -1,11 +1,13 @@
 from .user.views import router as user_router
 from .auth.views import router as auth_router
 from .base.views import router as base_router
+from .note.views import router as note_router
 
 def register_routes(app):
     """
     Register routes with blueprint and namespace
     """
     app.register_blueprint(base_router)
-    app.register_blueprint(auth_router) 
-    app.register_blueprint(user_router)       
+    app.register_blueprint(auth_router)
+    app.register_blueprint(user_router)
+    app.register_blueprint(note_router)
